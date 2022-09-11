2c8131b29d3dee5d9be9677e5c279516df535655
Zamahell <brau.herrerahuerta@gmail.com>
2022-09-11 05:23:07 -0300

    First Commit

---
+++ RutValidador.js
@@ -0,0 +1,58 @@
+class RutValidador {
+    //Obtener el rut con constructor para el ingreso del usuario
+    constructor(rut){
+        this.rut = rut;
+        //obtenemos el ultimo número (digito verificador) ingresado
+        this.dv = this.rut.substring(this.rut.length - 1);
+        //Limpiar el rut, quitando otros caracteres innecesarios
+        this.rut = this.rut.substring(0, this.rut.length - 1).replace(/\D/g, '')
+        this.esValido = this.validar();
+
+    }
+
+    validar(){
+        let numerosArray = this.rut.split('').reverse();
+        let acumulador = 0;
+        let multiplicador = 2;
+
+        for (let numero of numerosArray){
+            acumulador += parseInt(numero) * multiplicador;
+            multiplicador ++;
+
+            if (multiplicador == 8) {
+                multiplicador = 2;
+            }
+        }
+
+        let dv = 11 - (acumulador % 11 );
+
+        if (dv == 11)
+            dv = '0';
+        
+        if (dv == 10)
+            dv = 'k';
+        return dv == this.dv.toLowerCase()
+        
+    }
+
+    formato(){
+        if (!this.esValido) return '';
+
+        return (this.rut.toString().replace(/\B(?=(\d{3})+(?!\d))/g,'.')) + '-' + this.dv;
+    }
+
+}
+
+const Validador = new RutValidador(prompt("Ingresa tu rut con puntos y guion"))
+
+console.log(Validador.esValido)
+
+
+
+//PORFINNNN CTM APRENDI A VALIAR EN JS SEXOOOOOOOOO
+
+
+
+
+// console.log('Rut', Validador.rut);
+// console.log('DIGITO VD', Validador.dv);
